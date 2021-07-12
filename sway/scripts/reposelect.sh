f4fe337687f7a3219e133ce3ef91c1402fe2b2aa
Alfred Jophy <alfredjophy@protonmail.com>
2021-07-12 20:06:45 +0530

    .

--- sway/scripts/reposelect.sh
+++ sway/scripts/reposelect.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-repos=$(find Repos -name "*.git" -type d | sed -r 's#/.git##'| sed -r 's#Repos/##')
+repos=$(find ~/Repos -name "*.git" -type d -exec exa --icons -d {} \;| sed -r 's#/.git##'| sed -r 's#Repos/##'| sed -r 's#/home/ajrx/##')
 repo=$(echo "$repos" |wofi --show dmenu)
 
 [ -z "$repo" ] && exit
+echo $repo
+repo=$(echo "/home/ajrx/Repos/${repo:1}" | sed -r "s/ //")
 
-cd ~/Repos/$repo
+echo "$repo"
+cd $repo
 kitty -1 nvim . -c "NvimTreeOpen"
